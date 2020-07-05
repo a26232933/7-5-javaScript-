d5a0caf7a093cf1b231c8043ffcc69e3c8ffac02
a26232933 <a094868@gmail.com>
2020-07-05 12:46:44 +0800

    js

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("我是 JS!!!");
+
+// DOM 文件物件模式
+// 文件.透過 ID 搜尋元素("ID 名稱").插入 HTML = "文字訊息";
+document.getElementById("box1").innerHTML = "Hi~~~";
+
+// 變數 Variables
+// 用於儲存資料
+// 資料類型
+var a = 1 ;       //整數
+var b = 1.5  ;    //浮點數  
+var c = "文字" ;  //字串
+var d = true  ;   //布林值 ture 是 false 否
+
+console.log(a);
+
+// 運算子
+// 數學加減乘除 + - * /%
+
+// 程式內的 = 指定符號
+// 右邊先執行在指定給左邊
+a=a+1; 
+
+console.log(a);
